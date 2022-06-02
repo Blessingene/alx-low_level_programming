2de87e49f0420d80c169010780b4cf139021f62c
Blessingene <blessingene677@gmail.com>
2022-06-02 15:13:47 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
-
 #include <stdio.h>
 /*returns successful*/
 int main(void)
 {
-               puts("\"Programming is like building a multilingual puzzle");
-       	       return(0);
+               puts ("\"Programming is like building a multilingual puzzle");
+       	       return (0);
 }
 
